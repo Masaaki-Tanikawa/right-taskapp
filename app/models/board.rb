b9d408b0f370058a1a73938f0e4d443ae1169727
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-06-18 00:24:02 +0800

    board#indexビューの作成

--- app/models/board.rb
+++ app/models/board.rb
@@ -21,8 +21,8 @@ class Board < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
   validates :title, format: { with: /\A(?!\@)/ }
-  validates :content, presence: true
-  validates :content, length: { minimum: 5 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 5 }
 
   belongs_to :user
 end
