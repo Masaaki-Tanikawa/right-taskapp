f51d50ef83d885b56ea2d8cf4ba0ddb9f0807990
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-07-06 20:23:37 +0800

    Commentのビュー作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,26 @@
 class CommentsController < ApplicationController
   def new
-    card = Card.find(params[:card_id])
-    @comment = card.comments.build
+    @card = Card.find(params[:card_id])
+    @board = @card.board
+    @comment = @card.comments.build
   end
 
   def create
-    card = Card.find(params[:card_id])
-    @comment = card.comments.build(comment_params)
+    @card = Card.find(params[:card_id])
+    @board = @card.board
+    @comment = @card.comments.build(comment_params)
+
     if @comment.save
-      redirect_to board_card_path(card), notice: 'コメントを追加'
-		else
+      redirect_to board_card_path(@board, @card), notice: 'コメントを追加しました'
+    else
       flash.now[:error] = '更新できませんでした'
       render :new
     end
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:content)
-	end
+  end
 end
