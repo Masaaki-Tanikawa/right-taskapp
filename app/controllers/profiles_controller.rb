aa3753e5f0f0f12812aaa209109738a444e837bc
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-07-08 00:16:35 +0800

    Profileのビュー作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,7 @@ class ProfilesController < ApplicationController
     @profile = current_user.prepare_profile
     @profile.assign_attributes(profile_params)
     if @profile.save
-      redirect_to profile_path, notice: 'プロフィール更新！'
+      redirect_to profiles_path, notice: 'プロフィール更新！'
     else
       flash.now[:error] = '更新できませんでした'
       render :edit
