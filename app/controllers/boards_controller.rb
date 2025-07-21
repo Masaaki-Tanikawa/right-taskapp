808322acee27ddc3580261f2188eb2004f4f8864
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-07-22 07:24:10 +0800

    検索機能の実装・フィルターの修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,15 @@
 class BoardsController < ApplicationController
-  before_action :set_board, only: [ :edit, :update]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_board, only: [ :edit, :update ]
+  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
 
   def index
-  	@boards = Board.includes(:cards).all
+    @boards = Board.includes(:cards).all
+
+    if params[:q].present?
+      @boards = @boards.where("boards.title ILIKE ?", "%#{params[:q]}%")
+    end
   end
+
 
   def new
     @board = current_user.boards.build
@@ -13,9 +18,9 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to board_path(@board), notice: '保存できました'
+      redirect_to board_path(@board), notice: "\u4FDD\u5B58\u3067\u304D\u307E\u3057\u305F"
     else
-      flash.now[:error] = '保存に失敗しました'
+      flash.now[:error] = "\u4FDD\u5B58\u306B\u5931\u6557\u3057\u307E\u3057\u305F"
       render :new, status: :unprocessable_entity
     end
   end
@@ -25,9 +30,9 @@ class BoardsController < ApplicationController
 
   def update
     if @board.update(board_params)
-      redirect_to boards_path, notice: '更新できました'
+      redirect_to boards_path, notice: "\u66F4\u65B0\u3067\u304D\u307E\u3057\u305F"
     else
-      flash.now[:error] = '更新に失敗しました'
+      flash.now[:error] = "\u66F4\u65B0\u306B\u5931\u6557\u3057\u307E\u3057\u305F"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -35,7 +40,7 @@ class BoardsController < ApplicationController
   def destroy
     board = current_user.boards.find(params[:id])
     board.destroy!
-    redirect_to boards_path, status: :see_other, notice: '削除に成功しました'
+    redirect_to boards_path, status: :see_other, notice: "\u524A\u9664\u306B\u6210\u529F\u3057\u307E\u3057\u305F"
   end
 
   private
@@ -47,5 +52,4 @@ class BoardsController < ApplicationController
   def board_params
     params.require(:board).permit(:title, :description)
   end
-
 end
